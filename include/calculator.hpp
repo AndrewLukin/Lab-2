b8c94f43a02ff4a4e83a2766105b935274b4fb62
AndrewLukin <andreylukin3010@mail.ru>
2016-12-02 08:57:01 +0300

    Create calculator.hpp

---
+++ include/calculator.hpp
@@ -0,0 +1,6 @@
+double sum(double a, double b);
+double sub(double a, double b);
+double multip(double a, double b);
+double di(double a, double b);
+double power(double a, int c);
+double squareroot(double a);
