604370fc1592c16476a76d7145c2add99181d4a6
AndrewLukin <andreylukin3010@mail.ru>
2016-12-02 09:09:18 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -5,14 +5,30 @@ using namespace std;
 
 int main()
 {
-	double a=4, b=3;
-	int c=5;
+	double a,b;
+	int c;
+	while (1)
+	{
+		try
+		{
+			cin >> a;
+			cin >> b;
+			cin >> c;
+			if (b == 0)
+				throw b;
+
 			cout << sum(a, b) << endl;
 			cout << sub(a, b) << endl;
 			cout << multip(a, b) << endl;
 			cout << di(a, b) << endl;
 			cout << power(a, c) << endl;
 			cout << squareroot(a) << endl;
+		}
+		catch (double b)
+		{
+			cout << "Enable value for b\n";
+		}
+	}
  	system("pause");
 	return 0;
 }
