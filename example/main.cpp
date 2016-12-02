9ff8b36f88048786b7b91acfade519f0162d7237
AndrewLukin <andreylukin3010@mail.ru>
2016-12-02 09:07:10 +0300

    Create main.cpp

---
+++ example/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "calculator.hpp"
+
+using namespace std;
+
+int main()
+{
+	double a=4, b=3;
+	int c=5;
+			cout << sum(a, b) << endl;
+			cout << sub(a, b) << endl;
+			cout << multip(a, b) << endl;
+			cout << di(a, b) << endl;
+			cout << power(a, c) << endl;
+			cout << squareroot(a) << endl;
+ 	system("pause");
+	return 0;
+}
