2e67f54ee36ec83f41e6e7d6244e06499826abd2
AndrewLukin <andreylukin3010@mail.ru>
2016-12-09 14:34:18 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -30,7 +30,7 @@ SCENARIO("calculator multip", "[multip]") {
 }
 SCENARIO("calculator di", "[di]") {
     double a = 16.0;
-    double b = 8.0;
+    double b = 0.0;
     int c = 3;	
     
     double rv = di(a,b);
@@ -40,14 +40,14 @@ SCENARIO("calculator di", "[di]") {
 SCENARIO("calculator power", "[power]") {
     double a = 16.0;
     double b = 8.0;
-    int c = 3;
+    int c = -3;
 	
     double rv = power(a,c);
     
 	REQUIRE( rv == 4096.0 );
 }
 SCENARIO("calculator squareroot", "[squareroot]") {
-    double a = 16.0;
+    double a = -16.0;
     double b = 8.0;
     int c = 3;	
     
