52bb9086303f4cffd27558db07be060e823b4ff5
AndrewLukin <andreylukin3010@mail.ru>
2016-12-09 14:59:19 +0400

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -16,18 +16,24 @@ int main()
 			cin >> c;
 			if (b == 0)
 				throw b;
-
+        catch (double b)
+		{
+			cout << "Enable value for b\n";
+		}
 			cout << sum(a, b) << endl;
 			cout << sub(a, b) << endl;
 			cout << multip(a, b) << endl;
 			cout << di(a, b) << endl;
 			cout << power(a, c) << endl;
+			if (a < 0)
+				throw a;
+        catch (double a)
+		{
+			cout << "Enable value for a\n";
+		}			
 			cout << squareroot(a) << endl;
 		}
-		catch (double b)
-		{
-			cout << "Enable value for b\n";
-		}
+		
 	}
  	system("pause");
 	return 0;
