8f7e18673e893d20a9df6e4804538fa60b4c638f
AndrewLukin <andreylukin3010@mail.ru>
2016-12-09 14:55:34 +0400

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -28,30 +28,49 @@ SCENARIO("calculator multip", "[multip]") {
     
 	REQUIRE( rv == 128.0 );
 }
-SCENARIO("calculator di", "[di]") {
+SCENARIO("calculator dizero", "[dizero]") {
     double a = 16.0;
-    double b = 0.0;
-    int c = 3;	
+    double b = 0.0;	
     
     double rv = di(a,b);
     
-	REQUIRE( rv == 2.0 );
+	REQUIRE( rv == "Enable value for b" );
+}
+SCENARIO("calculator di", "[di]") {
+    double a = 16.0;
+    double b = 4.0;	
+    
+    double rv = di(a,b);
+    
+	REQUIRE( rv == 4 );
 }
 SCENARIO("calculator power", "[power]") {
     double a = 16.0;
-    double b = 8.0;
-    int c = -3;
+    int c = 3;
 	
     double rv = power(a,c);
     
 	REQUIRE( rv == 4096.0 );
 }
-SCENARIO("calculator squareroot", "[squareroot]") {
+SCENARIO("calculator powera", "[powera]") {
+    double a = 16.0;
+    int c = -3;
+	
+    double rv = power(a,c);
+    
+	REQUIRE( rv == 0.000244 );
+}
+SCENARIO("calculator squareroota", "[squareroota]") {
     double a = -16.0;
-    double b = 8.0;
-    int c = 3;	
     
     double rv = squareroot(a);
     	
-REQUIRE( rv == 4.0 );
+REQUIRE( rv == "Enable value for a" );
+}
+SCENARIO("calculator squareroot", "[squareroot]") {
+    double a = 16.0;
+    
+    double rv = squareroot(a);
+    	
+REQUIRE( rv == 4 );
 }
