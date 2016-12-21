1a2361c39d9cffd958543163cb666b1c0418734b
AndrewLukin <andreylukin3010@mail.ru>
2016-12-21 23:17:29 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -5,36 +5,23 @@ using namespace std;
 
 int main()
 {
-	double a,b;
+	double a, b;
 	int c;
-	while (1)
-	{
-		try
-		{
-			cin >> a;
-			cin >> b;
-			cin >> c;
-			if (b == 0)
-				throw b;
-        catch (double b)
-		{
-			cout << "Enable value for b\n";
-		}
-			cout << sum(a, b) << endl;
-			cout << sub(a, b) << endl;
-			cout << multip(a, b) << endl;
-			cout << di(a, b) << endl;
-			cout << power(a, c) << endl;
-			if (a < 0)
-				throw a;
-        catch (double a)
-		{
-			cout << "Enable value for a\n";
-		}			
-			cout << squareroot(a) << endl;
-		}
-		
-	}
- 	system("pause");
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	cout << sum(a, b) << endl;
+	cout << sub(a, b) << endl;
+	cout << multip(a, b) << endl;
+	long double result = di(a, b);
+	if (result != NULL)
+		cout << result << endl;
+	else
+		cout << "Invalid value for b" << endl;
+	cout << power(a, c) << endl;
+	long double result1 = squareroot(a);
+	if (result1) cout << squareroot(a) << endl;
+	else cout << "Invalid value for a" << endl;
+	system("pause");
 	return 0;
 }
