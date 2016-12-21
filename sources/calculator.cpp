aadba53fd718559f263c7a9a53ea2e7204b0ea4c
AndrewLukin <andreylukin3010@mail.ru>
2016-12-21 23:17:01 +0300

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -1,50 +1,56 @@
-double sum(double a,double b)
+double sum(double a, double b)
 {
-    return a+b;
+	return a + b;
 }
 double sub(double a, double b)
 {
-    return a-b;
+	return a - b;
 }
 double di(double a, double b)
 {
-	if(b==0)
-		throw "Enable value for b";
-    return a/b;
+	if (b != 0)
+	{
+		double *p = new double;
+		*p = a / b;
+		return *p;
+	}
+	else return NULL;
 }
 double multip(double a, double b)
 {
-    return a*b;
+	return a*b;
 }
 double power(double a, int c)
-{	
-        double a1 = 1;	
-        if (c>0)		
-                for (int i = 0; i < c; i++)		
-                        a1 = a1*a;	
-        if (c<0)	
-                for (int i = 0; i < (-c); i++)			
-                        a1 = a1 / a;
-        return a1;
+{
+	double a1 = 1;
+	if (c>0)
+		for (int i = 0; i < c; i++)
+			a1 = a1*a;
+	if (c<0)
+		for (int i = 0; i < (-c); i++)
+			a1 = a1 / a;
+	return a1;
 }
 double squareroot(double a)
 {
-	if(a<0)
-		throw "Enable value for a";
-	double xn = 1.0;  
+	double xn = 1.0;
 	double xn1 = 2.0;
-
-	if ( a!=1 && a!=0 )
+	if (a > 0)
 	{
-		while ((xn1-xn)>0.000001 || (xn1-xn)<-0.000001)
+		if (a != 1 && a != 0)
 		{
-		 xn=xn1;
-		 xn1=(float)(xn+(float)a/xn)/2;
+			while ((xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001)
+			{
+				xn = xn1;
+				xn1 = (float)(xn + (float)a / xn) / 2;
+			}
 		}
+		else
+			if (a == 1) xn1 = 1;
+			else
+				if (a == 0) xn1 = 0;
+		return xn;
 	}
-		else if (a==1) xn1=1;
-			else if (a==0) xn1=0;
-
-
-	return xn1;
+	else
+		return NULL;
 }
