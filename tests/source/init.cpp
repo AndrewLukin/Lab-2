60cdf0a4aef9401a201d3e09a892f1189ab6d6e7
AndrewLukin <andreylukin3010@mail.ru>
2016-12-22 08:05:58 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -2,55 +2,84 @@
 #include <catch.hpp>
 
 SCENARIO("calculator sum", "[sum]") {
-    float a = 9.0;
-    float b = 1.0;
+    float a = 7.0;
+    float b = 3.0;
     
     double rv = sum( a, b );
     
 	REQUIRE( rv == 10.0 );
 }
 SCENARIO("calculator sub", "[sub]") {
-    float a = 9.0;
-    float b = 1.0;
+    float a = 7.0;
+    float b = 3.0;
     
     double rv = sub( a, b );
     
-	REQUIRE( rv == 8.0 );
+	REQUIRE( rv == 4.0 );
 }
-SCENARIO("calculator mult", "[mult]") {
-    float a = 9.0;
-    float b = 1.0;
+SCENARIO("calculator multiplication", "[mult]") {
+    float a = 7.0;
+    float b = 3.0;
     
-    double rv = mult( a, b );
+    double rv = multip( a, b );
     
-	REQUIRE( rv == 9.0 );
+	REQUIRE( rv == 21.0 );
 }
-SCENARIO("calculator div", "[div]") {
-    float a = 1.0;
+SCENARIO("calculator di", "[di]") {
+    float a = 7.0;
     float b = 0.0;
     int* c = (int*)malloc(sizeof(int));
     
-    double *rv = div( a, b, c );
+    double *rv = di( a, b, c );
     
 	REQUIRE( rv == nullptr );
 }
-SCENARIO("calculator pow", "[pow]") {
-    float a = 2.0;
+SCENARIO("calculator di", "[di]") {
+    float a = 8.0;
+    float b = 4.0;
+    int* c = (int*)malloc(sizeof(int));
+    
+    double *rv = di( a, b, c );
+    
+	REQUIRE( rv == 2 );
+}
+SCENARIO("calculator power", "[power]") {
+    float a = 3.0;
     float b = 1.0;
-    int* extent = (int*)malloc(sizeof(int));
+    int* ext = (int*)malloc(sizeof(int));
+    int* c = (int*)malloc(sizeof(int));
+    *extent = 2;
+    
+    double rv = power( a, ext, c );
+    
+	REQUIRE( rv == 9 );
+}
+SCENARIO("calculator power", "[power]") {
+    float a = 4.0;
+    float b = 1.0;
+    int* ext = (int*)malloc(sizeof(int));
     int* c = (int*)malloc(sizeof(int));
     *extent = -1;
     
-    double rv = pow( a, extent, c );
+    double rv = power( a, ext, c );
     
-	REQUIRE( rv == 0.5 );
+	REQUIRE( rv == 0.25 );
 }
-SCENARIO("calculator sq", "[sq]") {
-    float a = -9.0;
+SCENARIO("calculator squareroot", "[squareroot]") {
+    float a = 4;
     float b = 1.0;
     int* c = (int*)malloc(sizeof(int));
     
-    double *rv = sq( a, c );
+    double *rv = squareroot( a, c );
+    
+	REQUIRE( rv == 2 );
+}
+SCENARIO("calculator squareroot", "[squareroot]") {
+    float a = -7.0;
+    float b = 1.0;
+    int* c = (int*)malloc(sizeof(int));
+    
+    double *rv = squareroot( a, c );
     
 	REQUIRE( rv == nullptr );
 }
