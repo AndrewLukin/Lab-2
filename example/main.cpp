2fce2c5c04b4ac99ef1b20c50e40a5edb573e209
AndrewLukin <andreylukin3010@mail.ru>
2016-12-22 07:39:32 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
-#include "calculator.hpp"
 
+#include "calculator.hpp"
 using namespace std;
 
 int main()
 {
-    	double a = 0.0;
-	double b = 0.0;
+    	float a = 0.0;
+	float b = 0.0;
 	int* c = (int*)malloc(sizeof(int));
 	int* extent = (int*)malloc(sizeof(int));
 	*c = 0;
 	*extent = 0;
 	cin >> a;
 	cin >> b;
-	cout << endl << sum(a,b) << endl;
-	cout << sub(a,b) << endl;
-	cout << mult(a,b) << endl;
+	cout << endl << "a + b = " << sum(a,b) << endl;
+	cout << "a - b = " << sub(a,b) << endl;
+	cout << "a * b = " << mult(a,b) << endl;
 	div(a,b,c);
-	if ( *c == 0 ) cout << div(a,b,c) << endl;
-		else cout << "Invalid value for b" << endl;
+	if ( *c == 0 ) cout << "a / b = " << div(a,b,c) << endl;
+		else cout << "a / b = Error: it can not be divided by zero" << endl;
+	cout << "a^e, print e = ";
 	cin >> *extent;
 	pow(a, extent, c);
-	if ( *c == 0 ) cout << pow(a, extent, c) << endl;
-	squareroot(a,c);
-	if ( *c == 0 ) cout << squareroot(a,c) << endl;
-		else cout << "Invalid value for a" << endl;
+	if ( *c == 0 ) cout << "a^e = " << pow(a, extent, c) << endl;
+		else cout << "a^e = Error (1/0): it can not be divided by zero" << endl;
+	sq(a,c);
+	if ( *c == 0 ) cout << "sqrt(a) = " << sq(a,c) << endl;
+		else cout << "sqrt(a) = Error: root of a negative number does not exist!" << endl;
+	
+	cout << endl << "Press any button to exit the program" << endl;
+
 	return 0;
 }
