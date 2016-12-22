220bdee6c670efef149b7bbf000ee5290e3c210b
AndrewLukin <andreylukin3010@mail.ru>
2016-12-22 07:58:46 +0300

    Update main.cpp

--- example/main.cpp
+++ example/main.cpp
@@ -1,34 +1,31 @@
 #include <iostream>
-
 #include "calculator.hpp"
+
 using namespace std;
 
 int main()
 {
-    	float a = 0.0;
-	float b = 0.0;
+    	float a = 3;
+	float b = 2;
 	int* c = (int*)malloc(sizeof(int));
-	int* extent = (int*)malloc(sizeof(int));
+	int* ext = (int*)malloc(sizeof(int));
 	*c = 0;
-	*extent = 0;
+	*ext = 0;
 	cin >> a;
 	cin >> b;
-	cout << endl << "a + b = " << sum(a,b) << endl;
-	cout << "a - b = " << sub(a,b) << endl;
-	cout << "a * b = " << mult(a,b) << endl;
-	div(a,b,c);
-	if ( *c == 0 ) cout << "a / b = " << div(a,b,c) << endl;
-		else cout << "a / b = Error: it can not be divided by zero" << endl;
-	cout << "a^e, print e = ";
-	cin >> *extent;
-	pow(a, extent, c);
-	if ( *c == 0 ) cout << "a^e = " << pow(a, extent, c) << endl;
-		else cout << "a^e = Error (1/0): it can not be divided by zero" << endl;
+	cout << sum(a,b) << endl;
+	cout << sub(a,b) << endl;
+	cout << multip(a,b) << endl;
+	di(a,b,c);
+	if ( *c == 0 ) cout << div(a,b,c) << endl;
+		else cout << "Invalid value for b" << endl;
+	cout << "a^e, e = ";
+	cin >> *ext;
+	power(a, ext, c);
+	if ( *c == 0 ) cout << pow(a, ext, c) << endl;
+		else cout << "Invalid value for a" << endl;
 	sq(a,c);
-	if ( *c == 0 ) cout << "sqrt(a) = " << sq(a,c) << endl;
-		else cout << "sqrt(a) = Error: root of a negative number does not exist!" << endl;
-	
-	cout << endl << "Press any button to exit the program" << endl;
-
+	if ( *c == 0 ) cout << squareroot(a,c) << endl;
+		else cout << "Invalid value for a" << endl;
 	return 0;
 }
