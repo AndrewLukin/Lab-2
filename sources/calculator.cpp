2c35e9995e52a71bc537186b4e7f0258fa081816
AndrewLukin <andreylukin3010@mail.ru>
2016-12-22 07:40:26 +0300

    Update calculator.cpp

--- sources/calculator.cpp
+++ sources/calculator.cpp
@@ -1,49 +1,49 @@
-double sum(double a, double b)
+double sum ( float a,  float b)
 {
 	return a + b;
 }
-double sub(double a, double b)
+double sub ( float a,  float b)
 {
 	return a - b;
 }
-double *di ( double a,  double b, int c)
+double mult ( float a,  float b)
 {
-	double d;
+	if ( a != 0 && b != 0 ) return a * b;
+		else if ( a == 0 || b == 0 ) return 0;
+}
+double *div ( float a,  float b, int* c)
+{
 	if ( a != 0 && b != 0  ) 
 	{ 
-
-		d = (float)a/b; 
+		double *d=new double; 
+		*d = (float)a/b; 
 		return d;
 	}
 		else if ( a == 0 ) return 0;
 		else 
 		{ 
-			c = 1; 
+			*c = 1; 
 			return nullptr; 
 		}
 }
-double multip(double a, double b)
-{
-	return a*b;
-}
-double power ( float a, int extent,  int c)
+double pow ( float a, int* extent,  int* c)
 {
 	float a2 = a;
 	
 	if ( a != 0 )
 	{
-		c = 0;
-		if ( extent < 0 )
+		*c = 0;
+		if ( *extent < 0 )
 	{
-		for (unsigned int i = 1; i < -extent; ++i)
+		for (unsigned int i = 1; i < -*extent; ++i)
 		{
 			a = a * a2;
 		}
 		a = (float)1 / a;
 	}
-	    else if ( extent > 0 ) 
+	    else if ( *extent > 0 ) 
 	    {
-			for (unsigned int i = 1; i < extent; ++i)
+			for (unsigned int i = 1; i < *extent; ++i)
 		    {
 			a = a * a2;
 		    }
@@ -52,19 +52,19 @@ double power ( float a, int extent,  int c)
 	
 	return (double) a;
 	}
-	else if ( a == 0 && extent < 0 ) c = 1;
-	else if ( a == 0 && extent > 0 ) return 0;
-	else if ( a == 0 && extent == 0 ) return 1;
+	else if ( a == 0 && *extent < 0 ) *c = 1;
+	else if ( a == 0 && *extent > 0 ) return 0;
+	else if ( a == 0 && *extent == 0 ) return 1;
 	
 }
-double squareroot ( double a, int c )
+double *sq ( float a, int* c )
 {
 	float xn = 1.0;  
 	float xn1 = 2.0;
-	double d;
+	
 	if ( a >= 0 )
 	{
-		c = 0;
+		*c = 0;
 		if ( a != 1 && a != 0 )
 	{
 		while ( (xn1 - xn) > 0.000001 || (xn1 - xn) < -0.000001  )
@@ -76,13 +76,14 @@ double squareroot ( double a, int c )
 	}
 		else if ( a == 1 ) xn1 = 1;
 			else if ( a == 0 ) xn1 = 0;
-		d = xn1; 
+	        double *d=new double; 
+		*d = xn1; 
 		return d;
 	}
 	
 	else 
 	{ 
-		c = 1; 
+		*c = 1; 
 		return nullptr;
  
 	}
